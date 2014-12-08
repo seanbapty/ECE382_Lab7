6ccd8e65becf1a8a02c2384b7a5462a43d003ff4
Sean Bapty <C16Sean.Bapty@usafa.edu>
2014-12-08 07:45:41 -0700

    updated comments

--- IRimplementation.c
+++ IRimplementation.c
@@ -17,20 +17,20 @@ unsigned int centerIR(){
 	ADC10CTL1 = INCH_4;								// center wired to pin 4
 	ADC10AE0 |= BIT4;
 	ADC10CTL0 |= ENC + ADC10SC;
-	return ADC10MEM;
+	return ADC10MEM;								// returns ADC value to be compared to find distance
 }
 
 unsigned int leftIR(){
 	ADC10CTL0 &= ~ENC;								// clear ADC prior to changing
-	ADC10CTL1 = INCH_5;								// center wired to pin 4
-	ADC10AE0 |= BIT5;
+	ADC10CTL1 = INCH_5;								// center wired to pin 5
+	ADC10AE0 |= BIT5;								// originally 3 but pin 3 didn't work
 	ADC10CTL0 |= ENC + ADC10SC;
 	return ADC10MEM;
 }
 
 unsigned int rightIR(){
 	ADC10CTL0 &= ~ENC;								// clear ADC prior to changing
-	ADC10CTL1 = INCH_2;								// center wired to pin 4
+	ADC10CTL1 = INCH_2;								// center wired to pin 2
 	ADC10AE0 |= BIT2;
 	ADC10CTL0 |= ENC + ADC10SC;
 	return ADC10MEM;
