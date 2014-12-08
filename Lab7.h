94de0f27a79dba43948d501fcf8b31517050ad44
Sean Bapty <C16Sean.Bapty@usafa.edu>
2014-12-08 07:35:17 -0700

    added basic functionality

---
+++ Lab7.h
@@ -0,0 +1,15 @@
+/*
+ * Lab7.h
+ *
+ *  Created on: Dec 3, 2014
+ *      Author: C16Sean.Bapty
+ */
+
+#ifndef LAB7_H_
+#define LAB7_H_
+
+unsigned int centerIR();
+unsigned int leftIR();
+unsigned int rightIR();
+
+#endif /* LAB7_H_ */
